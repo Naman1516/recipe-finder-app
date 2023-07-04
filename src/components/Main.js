a5f4c2d0f3a6622789fbc406c8796e986f522b1e
Naman <jawaliyan@gmail.com>
2023-07-04 15:07:42 +0530

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,34 +1,36 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import useRandomMeal from '../customHooks/useRandomMeal';
 import MealCard from './MealCard';
 import { API_URI } from '../utilities/constants';
 
 const Main = () => {
     const [searchText, setSearchText] = useState("");
+    const [meal, setMeal] = useState(null);
     const randomMeal = useRandomMeal();
 
-    const handleSearchInput = (e) => {
-    };
-
     const searchMeal = async () => {
-    };
+        try {
+            const data = await fetch(API_URI + "/search.php?s=" + searchText);
+            const json = await data.json();
 
-    useEffect(() => {
-        // Perform any side effects or initializations here
-    }, []);
+            setMeal(json?.meals[0]);
+        } catch (e) {
+            console.error(e);
+        }
+    };
 
     if (randomMeal === null) return;
 
-
-    const { idMeal: id, strMealThumb: thumbnail, strMeal: name, strCategory: category, strArea: area, strDrinkAlternative: drinkAlternative } = randomMeal;
+    const isRandom = !meal;
+    const { idMeal: id, strMealThumb: thumbnail, strMeal: name, strCategory: category, strArea: area, strDrinkAlternative: drinkAlternative } = meal || randomMeal;
 
     return (
         <div className='flex flex-col items-center mt-6'>
-            {/* <div className='flex justify-center items-center'>
-                <input type="text" placeholder={name} name="taskInput" value={searchText} onChange={handleSearchInput} className="input input-bordered input-primary w-full max-w-xs" />
+            <div className='flex justify-center items-center'>
+                <input type="text" placeholder={name} name="taskInput" value={searchText} onChange={(e) => setSearchText(e.target.value)} className="input input-bordered input-primary w-full max-w-xs" />
                 <button onClick={searchMeal} className='btn btn-primary ml-5'>Search</button>
-            </div> */}
-            <MealCard id={id} thumbnail={thumbnail} name={name} category={category} area={area} drinkAlternative={drinkAlternative} />
+            </div>
+            <MealCard id={id} thumbnail={thumbnail} name={name} category={category} area={area} drinkAlternative={drinkAlternative} isRandom={isRandom} />
         </div>
     );
 };
