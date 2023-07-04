1728f9e693f3531a1d1f84730c68403a23d5e5c6
Naman <jawaliyan@gmail.com>
2023-07-04 15:07:49 +0530

    Update MealCard.js

--- src/components/MealCard.js
+++ src/components/MealCard.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const MealCard = ({ id, thumbnail, name, category, area, drinkAlternative }) => {
+const MealCard = ({ id, thumbnail, name, category, area, drinkAlternative, isRandom }) => {
     return (
         <div className='mt-8 flex flex-col items-center justify-center rounded border border-solid border-primary p-8 m-8'>
             <img src={thumbnail} alt={name} className='rounded-full w-52' />
-            <Link to={"/category/" + category + "/" + id} className="text-4xl font-semibold pt-2 text-center hover:text-primary cursor-pointer">Have you tried '{name}' ?</Link>
+            <Link to={"/category/" + category + "/" + id} className="text-4xl font-semibold pt-2 text-center hover:text-primary cursor-pointer">
+                {isRandom ? (`Have you tried '${name}' ?`) : name}
+            </Link>
             <p className='pt-2 text-center'>Category: {category} | Area: {area} {drinkAlternative ? <>Drink Alternative: {drinkAlternative}</> : null}</p>
-        </div>
+        </div >
     );
 }
 
