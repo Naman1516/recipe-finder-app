93e2c37ce7ffef6fb73045c377a2f508dff0154b
Naman <jawaliyan@gmail.com>
2023-07-04 18:17:52 +0530

    Update MealCard.js

--- src/components/MealCard.js
+++ src/components/MealCard.js
@@ -3,13 +3,18 @@ import { Link } from 'react-router-dom';
 
 const MealCard = ({ id, thumbnail, name, category, area, drinkAlternative, isRandom }) => {
     return (
-        <div className='mt-8 flex flex-col items-center justify-center rounded border border-solid border-primary p-8 m-8'>
-            <img src={thumbnail} alt={name} className='rounded-full w-52' height="208px" width="208px" />
-            <Link to={"/category/" + category + "/" + id} className="text-4xl font-semibold pt-2 text-center hover:text-primary cursor-pointer">
-                {isRandom ? (`Have you tried '${name}' ?`) : name}
-            </Link>
-            <p className='pt-2 text-center'>Category: {category} | Area: {area} {drinkAlternative ? <>Drink Alternative: {drinkAlternative}</> : null}</p>
-        </div >
+        <div className="card bg-base-100 shadow-xl mt-8 h-96 w-96">
+            <figure>
+                <img src={thumbnail} alt={name} height="208px" width="208px" className='rounded w-full' />
+            </figure>
+            <div className="card-body">
+                <Link to={"/category/" + category + "/" + id} className="text-4xl font-semibold pt-2 text-center hover:text-primary cursor-pointer">
+                    {isRandom ? (`Have you tried '${name}' ?`) : name}
+                </Link>
+                <p className='pt-2 text-center'>Category: {category} | Area: {area} {drinkAlternative ? <>Drink Alternative: {drinkAlternative}</> : null}</p>
+
+            </div>
+        </div>
     );
 }
 
